8c8e7a9b8ecb01b74996fda2cf04283e20313284
NikhilNamade <nikhilnamade39@gmail.com>
2024-11-14 22:09:50 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ app.use(cors())
 app.use(express.json());
 app.use("/api/auth", require("./routes/auth"))
 app.use("/api/data", require("./routes/userdata"))
-app.use('/uploads', express.static(path.join(__dirname,"../uploads")));
+app.use('/uploads', express.static(path.join(__dirname,"./uploads")));
 
 // Connect to MongoDB
 connectTomongoose();
