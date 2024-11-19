97f5d0f43f9521f9ad0ebfd694dd2e71eb96b355
NikhilNamade <nikhilnamade39@gmail.com>
2024-11-19 11:22:48 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 
 // Update CORS configuration to allow the 'auth-token' header
 app.use(cors({
-  origin: 'https://railway-concession-system.onrender.com', // Allow frontend domain
+  origin: '*', // Allow frontend domain
   methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
   allowedHeaders: ['Content-Type', 'Authorization', 'auth-token'], // Add 'auth-token' to allowed headers
   credentials: true // If you need cookies or authentication headers
