fa8df5e0d6aa862e9c013f5e1bcfee6a7f678115
NikhilNamade <nikhilnamade39@gmail.com>
2024-11-19 10:54:28 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -4,11 +4,12 @@ const port = process.env.PORT || 5000;
 const connectTomongoose = require("./db")
 connectTomongoose()
 const cors = require('cors')
-app.use(express.json());
 app.use(cors({
   origin: 'https://railway-concession-system.onrender.com', // Allow frontend domain
-  methods: ['GET', 'POST', 'PUT', 'DELETE'] // Allowed HTTP methods
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
+  allowedHeaders: ['Content-Type', 'Authorization']
 }));
+app.use(express.json());
 app.use("/api/auth", require("./routes/auth"))
 app.use("/api/data", require("./routes/userdata"))
 
